b4c48bb4da1cf1b63e28b8e5d0f2f529ddf34845
Burak Kılıç <kilicburak@protonmail.com>
2021-11-26 17:46:26 +0300

    Create User Class

---
+++ UserAPI/Model/Users.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Model.UserAPI
+{
+    public class Users 
+    {
+        [Required(ErrorMessage = "{0} cannot be empty")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "{0} cannot be empty")]
+        [StringLength(16,ErrorMessage ="{0} cannot exceeds {1} characters")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "{0} cannot be empty")]
+        [StringLength(16,ErrorMessage ="{0} cannot exceeds {1} characters")]
+        public string Password { get; set; }
+        public string FullName { get; set; }
+    }
+}
